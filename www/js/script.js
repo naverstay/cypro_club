a25de4d28ee6a29d3b42521568ae8d8313716ba0
naverstay <3a4f044785G->
2016-09-15 11:22:50 +0300

    about

--- www/js/script.js
+++ www/js/script.js
@@ -1,7 +1,9 @@
-var callback_popup, body;
+var callback_popup, body, header, wnd, doc;
 
 $(function ($) {
-
+    header = $('.header');
+    doc = $(document);
+    wnd = $(window);
     body = $('body');
 
     callback_popup = $('#callback_popup').dialog({
@@ -23,10 +25,14 @@ $(function ($) {
 
     body.delegate('.callbackBtn', 'click', function () {
         callback_popup.dialog('open');
-
         return false;
     });
 
+    $('.mobMenuBtn').on ('click', function () {
+        body.toggleClass('open_menu');
+        return false;
+    });
+    
     $('.validateMe').validationEngine({
         scroll: false,
         showPrompts: false,
@@ -42,7 +48,31 @@ $(function ($) {
         addPromptClass: 'relative_mode',
         showOneMessage: false
     });
-    
+
+    $('.reviewSlider').each(function (ind) {
+        var sld = $(this);
+
+        sld.slick({
+            dots: true,
+            infinite: false,
+            arrows: true,
+            speed: 600,
+            zIndex: 1,
+            initialSlide: 0,
+            slide: '.slide',
+            prevArrow: sld.closest('.reviewHolder').find('.slider_prev'),
+            nextArrow: sld.closest('.reviewHolder').find('.slider_next'),
+            appendDots: sld.closest('.reviewHolder').find('.slider_dots'),
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            touchThreshold: 10
+        });
+    });
+
+    wnd.on('scroll', function () {
+        header.toggleClass('header_small', doc.scrollTop() > 60);
+    });
+        
     all_dialog_close();
 
 });
